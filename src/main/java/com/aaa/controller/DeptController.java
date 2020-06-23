a31e6a448246ce1c31606e89c552ca89ca9f48fb
你脸真肥 <1425080898@qq.com>
2020-06-23 19:22:38 +0800

    修改人：小刘

--- src/main/java/com/aaa/controller/DeptController.java
+++ src/main/java/com/aaa/controller/DeptController.java
@@ -16,12 +16,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
  * <p>
- * 部门表 前端控制器
+ * 部门表 前端控制器aaa
  * </p>
  *
  * @author Mr.Liu
